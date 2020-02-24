994518fc26d650167860f744c571178ba841ef12
yutapok <yutautsugi@outlook.com>
2020-02-24 22:37:36 +0900

    bugfix: initialize env var

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-(echo '#!'`which python3`; cat kubesible) > /usr/local/bin/kubesible
+(echo '#!'`which python`; cat kubesible) > /usr/local/bin/kubesible
 sudo chmod 755 /usr/local/bin/kubesible
 pip3 install -r requirement.txt
